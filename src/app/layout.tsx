fa8fe55c4c36eee862884dbb0b485097f59d4bdd
ashlee-seagle <ashlee-seagle10@gmail.com>
2026-07-12 10:25:44 -0400

    Add tutorial-based model gallery UI and related components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 
 import type { RootLayoutProps } from "./types";
 
-import Image from "next/image";
-import logo from "../../public/printforge-logo.png";
-import logoMobile from "../../public/printforge-logo-mobile.png";
+import Navbar from "./components/Navbar";
 
 const albertSans = Albert_Sans({
   subsets: ["latin"],
@@ -25,34 +22,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={`${albertSans.className} ${montserratAlternates.variable}`}
       >
-        <header className="w-full bg-white">
-          <nav className="flex justify-between px-6 py-4">
-            <Link href="/">
-              <div className="relative">
-                {/* Desktop logo */}
-                <Image
-                  src={logo}
-                  alt="PrintForge Logo"
-                  className="w-[200px] h-auto hidden md:block"
-                />
-                {/* Mobile logo */}
-                <Image
-                  src={logoMobile}
-                  alt="PrintForge Logo"
-                  className="w-[40px] h-auto block md:hidden"
-                />
-              </div>
-            </Link>
-            <ul className="flex items-center gap-2.5">
-              <li>
-                <Link href="/3d-models">3D Models</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <Navbar />
         {children}
       </body>
     </html>
