2347ed673813bdc4bb1d7a5533e8f94f8b77190b
ashlee-seagle <ashlee-seagle10@gmail.com>
2026-07-13 08:15:17 -0400

    feat: add category data helpers

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -11,6 +11,15 @@ export type Model = {
   dateAdded: string;
 }
 
+export type Category = {
+    displayName: string
+    slug: string
+}
+
+export type CategoriesData = {
+    categories: Category[]
+}
+
 // Page Types
 export type RootLayoutProps = Readonly<{
   children: React.ReactNode;
