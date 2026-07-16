9a9de418be5e8116458facc66dbda8e7510cf6b8
ashlee-seagle <ashlee-seagle10@gmail.com>
2026-07-16 08:40:40 -0400

    Refactor nav links to support active state

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import NavLink from "./NavLink";
 import Image from "next/image";
@@ -5,6 +7,8 @@ import logo from "../../../public/printforge-logo.png";
 import logoMobile from "../../../public/printforge-logo-mobile.png";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <header className="w-full bg-white">
       <nav className="flex justify-between px-6 py-4">
@@ -25,8 +29,12 @@ export default function Navbar() {
           </div>
         </Link>
         <ul className="flex items-center gap-2.5">
-          <NavLink href="/3d-models">3D Models</NavLink>
-          <NavLink href="/about">About</NavLink>
+          <NavLink href="/3d-models" isActive={pathname === "/3d-models"}>
+            3D Models
+          </NavLink>
+          <NavLink href="/about" isActive={pathname === "/about"}>
+            About
+          </NavLink>
         </ul>
       </nav>
     </header>
