d5a5670b7366965ec0c7310f30108f81ee2b87fd
ashlee-seagle <ashlee-seagle10@gmail.com>
2026-07-23 08:52:40 -0400

    feat: apply Roboto font globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import "./globals.css";
+import { Roboto } from "next/font/google";
 
 import Navbar from "./components/Navbar";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body>
         <Navbar></Navbar>
         {children}
